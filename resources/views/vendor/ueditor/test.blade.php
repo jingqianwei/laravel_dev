16c529df6ef1657e6fc61752ca04974b75dac6cf
jqw <jqw@qq.com>
2018-03-11 18:05:38 +0800

    搭建环境

---
+++ resources/views/vendor/ueditor/test.blade.php
@@ -0,0 +1,12 @@
+@include('vendor.ueditor.assets')
+
+<!-- 实例化编辑器 -->
+<script type="text/javascript">
+    var ue = UE.getEditor('container');
+    ue.ready(function() {
+        ue.execCommand('serverparam', '_token', '{{ csrf_token() }}'); // 设置 CSRF token.
+    });
+</script>
+
+<!-- 编辑器容器 -->
+<script id="container" name="content" type="text/plain"></script>
